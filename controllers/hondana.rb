55ae36f7187d66bc51fb089fc6a712006026aef3
Sho Hashimoto <hashimoto@shokai.org>
2013-02-07 22:29:27 +0900

    本棚演算で関連本検索を辿れるようにした  #2

--- controllers/hondana.rb
+++ controllers/hondana.rb
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
 get '/con/:shelf' do
   @shelf = params[:shelf]
+  @title = "#{@shelf}の本棚"
   CometIO.push :go, {:url => "http://hondana.org/#{@shelf}"}
   @books = Hondana.books @shelf
+  haml :con_hondana
+end
+
+get '/con/similar_books/:keyword' do
+  @keyword = params[:keyword]
+  @title = "「#{@keyword}」の関連本"
+  @books = Hondana.similar_books @keyword
   haml :con_hondana
 end
