aaf91035ba734f00ac0a3dfb7a95559ee92c4e8e
Sho Hashimoto <hashimoto@shokai.org>
2013-02-09 04:00:06 +0900

    アイテム詳細をTV側で表示、ただしAmazonは表示できない  #7

---
+++ public/js/con_contents_navi.js
@@ -0,0 +1,26 @@
+var io = new CometIO().connect();
+
+io.on("connect", function(session){
+  console.log("connect!! "+session);
+
+  $("#btn_up").click(function(){
+    io.push("scroll", {y : -100});
+  });
+  $("#btn_down").click(function(){
+    io.push("scroll", {y : 100});
+  });
+  $("#btn_left").click(function(){
+    io.push("scroll", {x : -100});
+  });
+  $("#btn_right").click(function(){
+    io.push("scroll", {x : 100});
+  });
+  $("#items img.item").click(function(e){
+    var url = e.currentTarget.attributes['x-url'].value;
+    io.push('go', {url: url});
+  });
+});
+
+io.on("error", function(err){
+  console.error(err);
+});
